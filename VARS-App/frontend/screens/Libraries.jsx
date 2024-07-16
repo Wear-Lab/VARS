054cb8966be653eb991a8bad27a1f704c4fddaa5
senporo <ke849611@ucf.edu>
2024-07-16 17:45:01 -0400

    Added Images and Edited Libaries.jsx
    Added two images to the images directory in frontend/screens. Did basic front end UI for Libraries.jsx

--- VARS-App/frontend/screens/Libraries.jsx
+++ VARS-App/frontend/screens/Libraries.jsx
@@ -1,21 +1,61 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, Image, StyleSheet, ScrollView } from "react-native";
+
+const folderIcon = require('./images/folder.png');
+const addIcon = require('./images/plus.png');
 
 const Libraries = () => {
-  
+
+  const ProtocolButton = ({ text, icon }) => {
+    return (
+      <View style={styles.button}>
+        <Image source={icon} style={styles.icon} />
+        <Text style={styles.text}>{text}</Text>
+      </View>
+    );
+  };
+
   return (
-    <View style={styles.page}>
-      <Text style={[{fontWeight: "bold"}]}>Libraries Page</Text>
-    </View>
+    <ScrollView contentContainerStyle={styles.page}>
+      <Text style={styles.title}>Library</Text>
+      <ProtocolButton text="Ex Protocol" icon={folderIcon} />
+      <ProtocolButton text="Ex Protocol" icon={folderIcon} />
+      <ProtocolButton text="Ex Protocol" icon={folderIcon} />
+      <ProtocolButton text="Ex Protocol" icon={folderIcon} />
+      <ProtocolButton text="Add Protocol" icon={addIcon} />
+    </ScrollView>
   );
 };
-
 const styles = StyleSheet.create({
   page: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  }
+    flexGrow: 1,
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: '#ffffff',
+  },
+  title: {
+    fontSize: 32,
+    marginBottom: 20,
+    marginTop: 50,
+  },
+  button: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 20,
+    marginVertical: 10,
+    backgroundColor: '#d3d3d3',
+    borderRadius: 5,
+    width: '120%',
+    alignSelf: 'center',
+  },
+  icon: {
+    width: 32,
+    height: 32,
+    marginRight: 10,
+  },
+  text: {
+    fontSize: 20,
+  },
 });
 
 export default Libraries;
